75cae2e0e45a25b9297cbadeb2c6f056c42bc8ca
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-12-29 01:56:19 +0100

    implement authentication first

--- server.js
+++ server.js
@@ -8,14 +8,30 @@ const app = express();
 
 app.use(helmet());
 
+function checkLoggedIn(req, res, next) {
+    const isLoggedin = true; //TODO
+    if (!isLoggedin) {
+        res.status(401).json({
+            error: "You must log in"
+        });
+    } 
+    next();
+}
+
+app.get('/auth/google/', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+    res.send('Your secret code is 22');
+});
+
 //set up a route to listen to the request 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '.', 'public', 'index.html'));
 });
-
-app.get('/secret', (req, res) => {
-    res.send('Your secret code is 22');
-})
 
 const PORT = 3000;
 http.createServer({
